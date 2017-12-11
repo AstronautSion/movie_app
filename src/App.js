496524bd901dffdfc01ecdc2dd87d499bdad8472
astronaut <astronaut.sion@gmail.com>
2017-12-11 22:52:29 +0900

    movie_app_4
    Ajax on React & Promises

--- src/App.js
+++ src/App.js
@@ -6,38 +6,13 @@ class App extends Component {
   //Render: componentWillMount()  ->  render()  ->  componentDidMount()
   //update: compomentWillReceiveProps()  ->  shouldComponentUpdate()  ->  componentWillUpdate()  ->  render()  ->  componentDidUpdate()
 
-
-  state ={
-  }
-
-
+  state ={}
+  
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies:[
-          {
-            title: "Matrix",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/51k1epcewKL.jpg"
-          },
-          {
-            title: "Hunger Games",
-            poster: "https://s-media-cache-ak0.pinimg.com/originals/e4/b7/f0/e4b7f0a895ee785af8d25fd34cc3c780.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster:  "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "http://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg"
-          },
-          {
-            title: "Inseption",
-            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI4NDk5NDgzN15BMl5BanBnXkFtZTcwNzU0OTk1Mw@@._V1_SY1000_CR0,0,685,1000_AL_.jpg"
-          }
-        ]
-      })
-    },5000);
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
--- src/Movie.js
+++ src/Movie.js
@@ -2,21 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-// class Movie extends Component {
-//     static propTypes = {
-//         title : PropTypes.string.isRequired,
-//         poster : PropTypes.string.isRequired
-//     }
-//     render(){
-//         return(
-//             <div>
-//             <MoviePoster poster={this.props.poster}/>
-//             <h1>{this.props.title}</h1>
-//             </div>
-//         );
-//     }
-// }
-
 function Movie({title, poster}){
     return (
         <div>
@@ -25,18 +10,6 @@ function Movie({title, poster}){
         </div>
     );
 }
-
-
-// class MoviePoster extends Component{  //Smart state, props있음
-//     static propTypes = {
-//         poster : PropTypes.string.isRequired
-//     }
-//     render(){
-//         return(
-//             <img src={this.props.poster} alt="Movie Poster" />
-//         );
-//     }
-// }
 
 function MoviePoster({poster}){  //Dumb ()  state없음 props 와 return만 존재함
     return (
