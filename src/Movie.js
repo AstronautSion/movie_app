ad04950578060af2126bd1032789b5e82c6ff0ec
astronaut <astronaut.sion@gmail.com>
2017-12-11 22:17:52 +0900

    Smart vs Dumb
    Smart Component vs Dumb Component

--- src/Movie.js
+++ src/Movie.js
@@ -1,37 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
+// class Movie extends Component {
+//     static propTypes = {
+//         title : PropTypes.string.isRequired,
+//         poster : PropTypes.string.isRequired
+//     }
+//     render(){
+//         return(
+//             <div>
+//             <MoviePoster poster={this.props.poster}/>
+//             <h1>{this.props.title}</h1>
+//             </div>
+//         );
+//     }
+// }
 
-    static propTypes = {
-        title : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-
-    render(){
-        return(
-            <div>
-            <MoviePoster poster={this.props.poster}/>
-            <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
 
-class MoviePoster extends Component{
+// class MoviePoster extends Component{  //Smart state, props있음
+//     static propTypes = {
+//         poster : PropTypes.string.isRequired
+//     }
+//     render(){
+//         return(
+//             <img src={this.props.poster} alt="Movie Poster" />
+//         );
+//     }
+// }
 
-    static propTypes = {
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} alt="Movie Poster" />
-        );
-    }
+function MoviePoster({poster}){  //Dumb ()  state없음 props 와 return만 존재함
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
 }
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+
+MoviePoster.propTypes = {
+    poster : PropTypes.string.isRequired
+}
+
 
 export default Movie;
