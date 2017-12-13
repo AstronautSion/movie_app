a7c0784b6aaad80b500214cba2dd89d9e0a140ec
astronaut <astronaut.sion@gmail.com>
2017-12-13 21:32:44 +0900

    movie_app_5
    Updating Movie

--- src/App.js
+++ src/App.js
@@ -15,7 +15,14 @@ class App extends Component {
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {      
       console.log(movie);
-      return <Movie title={movie.title_english} poster={movie.large_cover_image} key={index} />
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={index} 
+        genres ={movie.genres}
+        synopsis = {movie.synopsis}
+        
+      />
     })
     return movies;
   }
